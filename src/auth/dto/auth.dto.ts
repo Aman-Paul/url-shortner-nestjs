ac37d4b9c951a8ed96a664a5c865133c8d4d3a8f
Aman Paul <amanpaul155@gmail.com>
2024-03-17 00:05:32 +0530

    Implemented a global exceptional handler

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString, Min } from "class-validator"
+import { IsEmail, IsNotEmpty, IsOptional, IsString} from "class-validator"
 
 export class SignInDto {
     @IsEmail()
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './exceptions/global-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes( new ValidationPipe({
-    whitelist: true
+    whitelist: true,
+    stopAtFirstError: true
   }));
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(3333);
 }
 bootstrap();
