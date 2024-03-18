0f05745d5af5bf2d89fc782f560869aaaf3b2f4c
Aman Paul <amanpaul155@gmail.com>
2024-03-18 12:03:39 +0530

    Cron schedule to run every hour

--- src/url-shortener/url-shortener.schedular.ts
+++ src/url-shortener/url-shortener.schedular.ts
@@ -7,8 +7,8 @@ export class UserScheduler {
   constructor(private readonly urlShortner: UrlShortenerService) {}
 
   // @Cron('0 * * * * *') // runs every minutes
-  // @Cron('0 * * * *') // runs every hour
-@Cron('0 0 * * *') // runs every day at midnight
+  // @Cron('0 0 * * *') // runs every day at midnight
+@Cron('0 * * * *') // runs every hour
 async handleDeactivateInactiveUsers() {
     await this.urlShortner.deleteExpiredUrls();
   }
